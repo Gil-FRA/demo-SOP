fff238cf47180046775c85f64f78c8abc651899c
Gilberto A Fragoso Glez <gilafragoso@Gilbertos-MacBook-Air.local>
2025-05-02 14:45:55 -0600

    segunfo intento MAC

--- app/(app)/maestro/formulario-maestro.tsx
+++ app/(app)/maestro/formulario-maestro.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Button } from "@/app/_styles/components/ui/button";
+import { Button } from "@/app/_styles/components/ui/button"; 
 import { Input } from "@/app/_styles/components/ui/input";
 import {
   Select,
